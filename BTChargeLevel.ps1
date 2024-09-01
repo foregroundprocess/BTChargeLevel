76da6343cabdeec2f98e97861f3a775b509fe467
foregroundprocess <iryazanov@gmail.com>
2024-09-01 18:44:43 +0500

    Add connectivity info to the skin. Minor fixes.

--- BTChargeLevel.ps1
+++ BTChargeLevel.ps1
@@ -2,24 +2,29 @@
 #Change the value below to a name of your device exactly as it is called in the Bluetooth system settings
 $BTDeviceFriendlyName = "EDIFIER WH950NB"
 
-$Shell = New-Object -ComObject "WScript.Shell"
 $BTHDevices = Get-PnpDevice -FriendlyName "*$($BTDeviceFriendlyName)*"
+$BTChargeLevel = $null
+$IsConnected = $false
 
 if ($BTHDevices) {
-    $BatteryLevels = foreach ($Device in $BTHDevices) {
-        $BatteryProperty = Get-PnpDeviceProperty -InstanceId $Device.InstanceId -KeyName '{104EA319-6EE2-4701-BD47-8DDBF425BBE5} 2' |
-            Where-Object { $_.Type -ne 'Empty' } |
-            Select-Object -ExpandProperty Data
-
-        if ($BatteryProperty) {
-            $BatteryProperty
+    foreach ($Device in $BTHDevices) {
+        $BTDeviceProperties = Get-PnpDeviceProperty -InstanceId $Device.InstanceId
+        foreach ($BTDeviceProperty in $BTDeviceProperties){
+            if (($BTDeviceProperty.KeyName -eq "{104EA319-6EE2-4701-BD47-8DDBF425BBE5} 2") -and ($null -ne $BTDeviceProperty.Data)){
+                $BTChargeLevel = $BTDeviceProperty.Data
+            }            
+        
+            if (($BTDeviceProperty.KeyName -eq "{83DA6326-97A6-4088-9453-A1923F573B29} 15") -and ($null -ne $BTDeviceProperty.Data)){
+                $IsConnected = $BTDeviceProperty.Data            
+            }
         }
     }
 
-    if ($BatteryLevels) {
+    if ($null -ne $IsConnected) {
         if (!(Test-Path HKCU:\Software\ForegroundProcess\BTChargeLevel)) {
             New-Item -Path HKCU:\Software\ForegroundProcess\BTChargeLevel -Force
         }
-        Set-ItemProperty -Path HKCU:\Software\ForegroundProcess\BTChargeLevel -Name BTChargeLevel -Value $BatteryLevels
+        Set-ItemProperty -Path HKCU:\Software\ForegroundProcess\BTChargeLevel -Name BTChargeLevel -Value $BTChargeLevel
+        Set-ItemProperty -Path HKCU:\Software\ForegroundProcess\BTChargeLevel -Name IsConnected -Value $IsConnected
     }
 }
